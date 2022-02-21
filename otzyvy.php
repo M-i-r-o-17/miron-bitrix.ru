e5eb9c23d4d0b1fff8d812fa93df517e92529091
M-i-r-o-17 <m_i_r_o_17@vk.com>
2022-02-21 17:23:53 +0900

    Небольшие изменения на страницу "Отзывы". Изучены ShowTitle и ShowProperty

--- otzyvy.php
+++ otzyvy.php
@@ -5,4 +5,13 @@ $APPLICATION->SetPageProperty("test", "Просто для проверки во
 $APPLICATION->SetPageProperty("keywords_inner", "Тестим, заполнение, meta, тегов");
 $APPLICATION->SetPageProperty("title", "Отзывы | Тестовая работа");
 $APPLICATION->SetTitle("Отзывы");
-?>Текст....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+<!--Тело страницы-->
+<h1>Title - <?=$APPLICATION->ShowTitle()?></h1>
+<h3>Title - <?=$APPLICATION->ShowTitle(false)?></h3>
+<hr/>
+<p>Tags - <?=$APPLICATION->ShowProperty("tags")?></p>
+<p>Test - <?=$APPLICATION->ShowProperty("test")?></p>
+<p>Keywords - <?=$APPLICATION->ShowProperty("keywords_inner")?></p>
+<p>Title property - <?=$APPLICATION->ShowProperty("title")?></p>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
